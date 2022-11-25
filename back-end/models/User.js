7f727e08826a173515f1763c3fa909264f5e7a4f
Emmanuelikeh <88993873+Emmanuelikeh@users.noreply.github.com>
2022-11-25 13:12:02 -0600

    back_end login commit

---
+++ back-end/models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
+
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required : [true, "This field cannot be empty "]
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required : [true, "This field cannot be empty "],
+        index: true,
+        validate: [isEmail, "invalid email"]
+    },
+    password: {
+        type: String,
+        required : [true, "This field cannot be empty "]
+    },
+    newProject:{
+        type: Object,
+        default: {} 
+    }
+}, {minimize:false});
+
+UserSchema.methods.toJSON = function(){
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+}
+
+UserSchema.statics.findByCredentials = async function(email, password){
+    const user = await User.findOne({email});
+    if(!user) throw new Error("invalid email or password");
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) throw new Error("invalid email or password");
+    // if any throws an error it does not return
+    return user
+}
+
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User
--- back-end/server.js
+++ back-end/server.js
@@ -7,6 +7,32 @@ app.use(express.json());
 app.use(cors());
 
 
+// User schema
+const User = require("./models/User")
+
+// `npm install mongoose`
+const mongoose = require("mongoose");
+const options = {
+    keepAlive: true,
+    connectTimeoutMS: 10000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+const dbUrl = `mongodb+srv://admin:lDwDH1ijgV95YOd8@cluster0.hylimuo.mongodb.net/projectManagementAppMern?retryWrites=true&w=majority`;
+
+
+mongoose.connect(dbUrl, options, (err) => {
+  if (err) console.log(err);
+});
+
+// Validate DB connection
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Mongo DB Connected successfully");
+});
+
 
 const server = require("http").createServer(app);
 const PORT = 5001;
@@ -18,6 +44,31 @@ const io = require("socket.io")(server, {
 });
 
 
+app.post('/create', async(req, res) => {
+  try{
+    const {name, email, password} = req.body;
+    console.log(req.body);
+    const user = await User.create({name, email, password});
+    res.status(201).json(user)
+  }
+  catch(err){
+    res.status(400).json({
+      status:400,
+      message: err.message,
+    })
+  }
+})
+
+app.post('/login', async(req, res) => {
+  try{
+    const {email, password} = req.body;
+    const user = await User.findByCredentials(email, password);
+    res.status(200).json(user);
+  }
+  catch(err){
+    res.status(400).json(err.message)
+  }
+})
 
 
 

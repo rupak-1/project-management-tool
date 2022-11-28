79dff142dc00be693e1f6b3153443c8c044b22f1
Emmanuelikeh <88993873+Emmanuelikeh@users.noreply.github.com>
2022-11-28 08:57:15 -0600

    updated to modal commit

--- front_end/src/components/Example.jsx
+++ front_end/src/components/Example.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import {FloatingLabel, Form} from 'react-bootstrap'
 
 function Example() {
+  {/* this value takes in a prop and reders the */}
   const [show, setShow] = useState(false);
-
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("")
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
@@ -13,9 +16,28 @@ function Example() {
       <i variant="primary" onClick={handleShow} className="fa-solid fa-pen-to-square"/>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Edit Task</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+        <>
+        <FloatingLabel
+        controlId="floatingTextarea"
+        label="Title"
+        className="mb-3"
+      >
+      <Form.Control as="textarea" placeholder="Leave a comment here" value={title} onChange={e => setTitle(e.target.value)} />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingTextarea2" label="Description">
+        <Form.Control
+          as="textarea"
+          value={description}
+          placeholder="Leave a comment here"
+          style={{ height: '100px' }}
+          onChange={e => setDescription(e.target.value)}
+        />
+      </FloatingLabel>
+        </>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
